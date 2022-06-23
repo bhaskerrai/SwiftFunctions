570cdc3415062e7956ba466c26e87adb4b567bb2
bhaskerrai <brai285@gmail.com>
2022-06-23 22:48:13 +0500

    Completed Chapter

--- SwiftFunctions.playground/Contents.swift
+++ SwiftFunctions.playground/Contents.swift
@@ -34,14 +34,14 @@ print(myFunc1(name: "Raj", nclass: 9))
 
 //here is an example of a function that doesn't have a return statement in it.
 
-func myfunc2(n:String){
+func myFunc2(n:String){
     print("\(n) is a great company!")
 }
 //“Because it doesn’t need to return a value, the function’s definition doesn’t include the return arrow (->) or a return type.”
 
 //“Strictly speaking, this function does still return a value, even though no return value is defined. Functions without a defined return type return a special value of type Void. This is simply an empty tuple, which is written as ().”
 
-myfunc2(n: "Apple")
+myFunc2(n: "Apple")
 
 
 //“The return value of a function can be ignored when it’s called:”
@@ -71,24 +71,25 @@ print(printWithoutCounting(string: "hello, world"))
 
 
 
-
 //“Functions with Multiple Return Values
 //You can use a tuple type as the return type for a function to return multiple values as part of one compound return value.”
 
 func myFunc3(movie:String, year:Int) -> (m: String, y:Int){
     print("Your searched for: ")
-    var m = movie
-    var y = year
+    let m = movie
+    let y = year
     return (m, y)
 }
 
 print(myFunc3(movie: "Inception", year: 2010))
 
 
- func minMax(array: [Int]) -> (min: Int, max: Int) {
+func minMax(array: [Int]) -> (min: Int, max: Int) {
+    
     var currentMin = array[0]
     var currentMax = array[0]
     for value in array[1..<array.count] {
+        
         if value < currentMin {
             currentMin = value
         } else if value > currentMax {
@@ -215,4 +216,153 @@ myFunc8(noDefaultValue: 56)
 
 //you can also assign a value to the parameter with a default value.
 myFunc8(noDefaultValue: 56,defaultValue: 77)
+
+
+
+
+
+//Variadic Parameters
+//A variadic parameter accepts zero or more values of a specified type. You use a variadic parameter to specify that the parameter can be passed a varying number of input values when the function is called. Write variadic parameters by inserting three period characters (...) after the parameter’s type name.
+
+func myFunc9(_ num: Float...)->Float{
+    
+    var total: Float = 0
+    
+    for n in num{
+        total += n
+    }
+    return total/Float(num.count)
+}
+
+print(myFunc9(1,3,9,3,2,4))
+//we can give any number of parameters.
+print(myFunc9(44,55,66,77,88,99,11,22))
+
+func myFunc10(num: Int...){
+    for i in num{
+        print("The square of \(i) is: \(i * i)")
+
+    }
+}
+
+myFunc10(num:2,3,4,5,6)
+myFunc10(num:2,3,4,5,8,9,10)
+
+//A function can have multiple variadic parameters. The first parameter that comes after a variadic parameter must have an argument label. The argument label makes it unambiguous which arguments are passed to the variadic parameter and which arguments are passed to the parameters that come after the variadic parameter.
+
+
+
+//“In-Out Parameters
+//Function parameters are constants by default. Trying to change the value of a function parameter from within the body of that function results in a compile-time error. This means that you can’t change the value of a parameter by mistake. If you want a function to modify a parameter’s value, and you want those changes to persist after the function call has ended, define that parameter as an in-out parameter instead.
+
+//You write an in-out parameter by placing the inout keyword right before a parameter’s type.”
+//You place an ampersand (&) directly before a variable’s name when you pass it as an argument to an in-out parameter, to indicate that it can be modified by the function.”
+
+print("\n")
+var n = 40
+print("The original value of variable 'n' is:",n)
+
+
+func myFunc10(i: inout Int){
+    
+    i *= 2 //doubles the value
+}
+
+myFunc10(i:&n)
+print("The modified value of variable 'n' is now:",n)
+
+
+
+//Using Function Types
+//Every function has a specific function type, made up of the parameter types and the return type of the function.
+//You use function types just like any other types in Swift. For example, you can define a constant or variable to be of a function type and assign an appropriate function to that variable:
+
+func myFunc11(_ a: Float, _ b:Float) -> Float{
+    
+    return a / b
+}
+
+var div: (Float,Float) -> Float = myFunc11
+
+print("dividing 21 by 5 we get: \(div(21,5))")
+
+//“A different function with the same matching type can be assigned to the same variable, in the same way as for nonfunction types:”
+
+func myFunc12(_ a: Float, _ b:Float) -> Float{
+    
+    return a.remainder(dividingBy: b) //you cannot use modulo on Float64 but on Int only; therefore we use either remainder() or truncatingRemainder().
+}
+
+//div = myFunc12
+//print("the remainder we get on dividing 21 by 5 is: \(div(21,5))")
+
+
+
+//As with any other type, you can leave it to Swift to infer the function type when you assign a function to a constant or variable:
+let var1 = myFunc12
+
+
+//Function Types as Parameter Types
+//You can use a function type such as (Int, Int) -> Int as a parameter type for another function. This enables you to leave some aspects of a function’s implementation for the function’s caller to provide when the function is called.
+
+
+func myFunc13(_ myFunc11:(Float, Float) -> Float, _ a: Float, _ b: Float){
+
+    print("the answer is: \(myFunc11(a, b))")
+}
+
+myFunc13(div, 30, 6)
+
+
+func stepForward(_ input: Int) -> Int {
+    return input + 1
+}
+func stepBackward(_ input: Int) -> Int {
+    return input - 1
+}
+
+
+func chooseStepFunction(backward: Bool) -> (Int) -> Int {
+    return backward ? stepBackward : stepForward
+}
+
+var currentValue = 3
+let moveNearerToZero = chooseStepFunction(backward: currentValue > 0)
+// moveNearerToZero now refers to the stepBackward() function
+
+print("Counting to zero:")
+// Counting to zero:
+
+while currentValue != 0 {
+    print("\(currentValue)... ")
+    currentValue = moveNearerToZero(currentValue)
+    
+}
+
+print("zero!")
+
+// 3...
+// 2...
+// 1...
+// zero!
+
+
+//Nestesd Functions:
+
+print("\n")
+func chooseStepFunction1(backward: Bool) -> (Int) -> Int {
+    func stepForward(input: Int) -> Int { return input + 1 }
+    func stepBackward(input: Int) -> Int { return input - 1 }
+    return backward ? stepBackward : stepForward
+}
+
+var currentValueNew = -4
+let moveNearerToZeroAgain = chooseStepFunction1(backward: currentValueNew > 0)
+// moveNearerToZero now refers to the nested stepForward() function
+while currentValueNew != 0 {
+    print("\(currentValueNew)... ")
+    currentValueNew = moveNearerToZeroAgain(currentValueNew)
+}
+print("zero!")
+
 
